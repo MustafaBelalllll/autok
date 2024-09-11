6eee91f42bc205db95662252b50f2af3fde17786
Mustafa Belal <144907897+MustafaBelalllll@users.noreply.github.com>
2024-09-11 13:03:23 +0200

    Delete feladat directory

--- feladat/node.js
+++
@@ -1,9 +0,0 @@
-const autok = ['mercedes', 'bmw', 'audi', 'chevrolette', 'subaru', 'ferrai']
-function init() {
-    document.getElementById('car').innerHTML = autok.map( v =>
-        `<div onclick="f('${v}')">${v}</div>`    
-    ).join("")
-}
-function f (a) {
-    console.log(a)
-}
